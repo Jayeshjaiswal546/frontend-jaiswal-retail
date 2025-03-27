4231599ce0cbb7c0dc122f98d4964445694e37a2
jayeshjaiswal546 <jayeshjaiswal546@gmail.com>
2025-03-28 02:09:56 +0530

    Fixing LocalStorage Error

--- src/app/redux/reducers/userSlice.js
+++ src/app/redux/reducers/userSlice.js
@@ -1,31 +1,37 @@
 
 import { createSlice } from '@reduxjs/toolkit'
+// const initialState = {
+//     token: JSON.parse(localStorage.getItem('jaiswal-retail-userToken')),
+//     // userCart: [],
+//   }
+
 const initialState = {
-    token: JSON.parse(localStorage.getItem('jaiswal-retail-userToken')),
-    // userCart: [],
-  }
+  token: typeof window !== "undefined"
+    ? JSON.parse(localStorage.getItem('jaiswal-retail-userToken'))
+    : null,
+};
 
 
-  export const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-      addUserToken: (state,action) => {
-        state.token = action.payload;
-      },
-      removeUserToken: (state) => {
-        state.token = null
-      },
-      // addUserCart: (state,action)=>{
-      //   state.userCart = action.payload;
-      // },
-      // removeUserCart: ()=>{
-      //   state.userCart = [];
-      // }
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    addUserToken: (state, action) => {
+      state.token = action.payload;
     },
-  })
-  
-  // Action creators are generated for each case reducer function
-  export const { addUserToken, removeUserToken,} = userSlice.actions
-  
-  export default userSlice.reducer;
+    removeUserToken: (state) => {
+      state.token = null
+    },
+    // addUserCart: (state,action)=>{
+    //   state.userCart = action.payload;
+    // },
+    // removeUserCart: ()=>{
+    //   state.userCart = [];
+    // }
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { addUserToken, removeUserToken, } = userSlice.actions
+
+export default userSlice.reducer;
