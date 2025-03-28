e1594a5ef694bbefc125f512905a633da65edf1a
Jayeshjaiswal546 <139918759+Jayeshjaiswal546@users.noreply.github.com>
2025-03-28 18:37:43 +0530

    Update api.js

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -1,5 +1,8 @@
 // exports.backendBaseUrl = 'http://localhost:8000';
 
 
-exports.backendBaseUrl = 'https://server-jaiswal-retail-production.up.railway.app';
+// exports.backendBaseUrl = 'https://server-jaiswal-retail-production.up.railway.app';
+exports.backendBaseUrl = 'server-jaiswal-retail-production-2619.up.railway.app
+';
+
 
